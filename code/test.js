7878416d1768a06c4bf7fd61868183a9cb515cd8
信管201张辉 <3133642579@qq.com>
2023-02-25 22:14:01 +0800

    Update test.js

--- code/test.js
+++ code/test.js
@@ -1,4 +1,4 @@
-var a = document.getElementsByClassName("container")[0];
+var a = document.getElementsByClassName("container");
 a.style.width="100px";
 a.style.height="100px";
 a.style.background="red";
